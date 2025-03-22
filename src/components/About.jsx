fef7d8cddc7c4fbaea517685e71cec352146a588
Tusharbiley <94297216+Tusharbiley@users.noreply.github.com>
2025-03-22 19:20:49 +0530

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="About"
+      className="max-w-screen-2xl container mx-auto px-4 md:px-20 my-20"
+    >
+      <div>
+        <h1 className="text-3xl font-bold mb-5">About</h1>
+        <p>
+          Hello, I'm Tushar, a passionate Web developer. With a background in
+          IT, I strive to create impactful and visually stunning Software
+          solutions that leave a lasting impression.
+        </p>
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Education & Training
+        </h1>
+        <span>
+          Pursuing B.Tech in Computer science engineering , currently in
+          2nd year. 
+          Enrolled in online courses to enhance web development
+          skills.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Skills & Expertise
+        </h1>
+        <span>
+          Proficient in HTML, CSS, and beginner-level JavaScript. Learning
+          React.js and backend development.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Professional Experience
+        </h1>
+        <span>
+          Currently exploring web development through personal projects and
+          online learning.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Achievements & Awards
+        </h1>
+        <span>
+          Successfully completed foundational courses in web development. Built
+          small projects showcasing HTML, CSS, and JavaScript skills.
+        </span>
+        <br />
+        <br />
+        <h1 className="text-green-600 font-semibold text-xl">
+          Mission Statement
+        </h1>
+        <p>
+          My mission is to leverage my skills and creativity to deliver
+          innovative solutions that exceed client expectations and
+          contribute positively to the digital landscape. I am committed to
+          continuous learning and growth, always seeking new challenges and
+          opportunities to expand my horizons.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
